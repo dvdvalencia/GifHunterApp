43e7d07e6d5dafb8b3702a68db8510e2e3b0fcd0
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2026-04-30 22:27:24 -0500

    GifsApp.test.tsx configuracion pruebas (testing)

---
+++ src/GifsApp.test.tsx
@@ -0,0 +1,20 @@
+// import { describe, test } from "vitest";
+
+// describe("GifsApp", () => {
+//   test("should render component properly", () => {
+//     //
+//   });
+// });
+
+import { describe,expect, test } from "vitest";
+import { render } from "@testing-library/react";
+import { GifsApp } from "./GifsApp";
+
+
+describe('GifsApp', () => {
+  test('should render component properly', () => {
+    const { container} = render ( <GifsApp />)
+
+    expect (container).toMatchSnapshot();
+  });
+});
