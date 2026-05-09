c9c3c8e4038107c17afa32dfc8b58a27b64eb271
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2026-05-08 23:35:35 -0500

    Continuacion CustomHeader.test.tsx (testing)

--- src/shared/components/interfaces/CustomHeader.test.tsx
+++ src/shared/components/interfaces/CustomHeader.test.tsx
@@ -3,18 +3,38 @@ import { render, screen } from "@testing-library/react";
 import { CustomHeader } from "../CustomHeader";
 
 describe("CustomHeader", () => {
+  const title = "Test Title";
+
   test("should render the title correctly", () => {
+    render(<CustomHeader title={title} />);
 
-    const title="Test Title"
-    render (<CustomHeader title={ title } />)
-
-    screen.debug();
-    // expect CustomHeader().props
-    // expect(screen.)
+    expect(screen.getByText(title)).toBeDefined;
+    // screen.debug();
   });
-  test("should render the description when provider ", () => {});
+  test("should render the description when provider ", () => {
+    const description = "Test Description";
+
+    render(<CustomHeader title={title} description={description} />);
+
+    // screen.debug();
+
+
+    expect(screen.getByText((description))).toBeDefined();
+    expect(screen.getByRole('paragraph')).toBeDefined();
+    expect(screen.getByRole('paragraph').innerHTML).toBe(description);
+  });
   test("should render the description when not provider", () => {
+
+    const {container} = render(<CustomHeader title={title} />)
+
+    const divElement = container.querySelector('.conten-center')
+
+    const h1 = divElement?.querySelector('h1');
+    
+    expect(h1?.innerHTML).toBe(title);
+
+    // screen.debug();
+
     //container = render()
   });
 });
- 
