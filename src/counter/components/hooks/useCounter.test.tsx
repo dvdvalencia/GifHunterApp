5be7800499485120a96a7461d217e65b30c9f713
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2026-05-21 01:05:23 -0500

    All methods - useCounter.test.tsx (testing)

--- src/counter/components/hooks/useCounter.test.tsx
+++ src/counter/components/hooks/useCounter.test.tsx
@@ -1,12 +1,45 @@
 import { describe, expect, test } from "vitest";
 import { useCounter } from "./useCounter";
-import { renderHook } from "@testing-library/react";
-
-
+import { act, renderHook } from "@testing-library/react";
 
 describe("useCounter", () => {
-  test ('should initialize whit default value of 10', () => {
-    const {result} = renderHook(() => useCounter());
-    expect(result.current.counter).toBe(10)
+  test("should initialize whit default value of 10", () => {
+    const { result } = renderHook(() => useCounter());
+    expect(result.current.counter).toBe(10);
+  });
+
+  test("should initialize whit value 20", () => {
+    const { result } = renderHook(() => useCounter(20));
+    expect(result.current.counter).toBe(20);
+  });
+
+  test("shoul increment counter when handleAdd is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.handleAdd();
+    });
+
+    expect(result.current.counter).toBe(11);
+  });
+
+  test("shoul subtract counter when handleSubtract is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.handleSubtract();
+    });
+
+    expect(result.current.counter).toBe(9);
+  });
+
+  test("shoul reset counter when handleReset is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.handleReset();
+    });
+
+    expect(result.current.counter).toBe(10);
   });
 });
