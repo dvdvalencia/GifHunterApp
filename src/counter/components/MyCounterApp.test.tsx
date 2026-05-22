57c866dd8ff604f10f262cea291abccac781c8c0
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2026-05-21 23:33:55 -0500

    All the methods - MyCounterApp.test.tsx (testing)

--- src/counter/components/MyCounterApp.test.tsx
+++ src/counter/components/MyCounterApp.test.tsx
@@ -1,18 +1,48 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { MyCounterApp } from "./MyCounterApp";
 
 describe("MyCounterApp", () => {
   test("should render the componnt", () => {
     render(<MyCounterApp />);
-    screen.debug();
+    // screen.debug();
 
-    expect(screen.getByRole('heading', { level: 1 }).innerHTML).toContain(
+    expect(screen.getByRole("heading", { level: 1 }).innerHTML).toContain(
       `counter: 10`,
     );
 
-    expect(screen.getByRole('button', {name: '+1'})).toBeDefined();
-    expect(screen.getByRole('button', {name: '-1'})).toBeDefined();
-    expect(screen.getByRole('button', {name: 'Reset'})).toBeDefined();
+    expect(screen.getByRole("button", { name: "+1" })).toBeDefined();
+    expect(screen.getByRole("button", { name: "-1" })).toBeDefined();
+    expect(screen.getByRole("button", { name: "Reset" })).toBeDefined();
+  });
+
+  test("should increment the counter", () => {
+    render(<MyCounterApp />);
+
+    const labelH1 = screen.getByRole("heading", { level: 1 });
+    const button = screen.getByRole("button", { name: "+1" });
+
+    fireEvent.click(button);
+    expect(labelH1.innerHTML).toContain("counter: 11");
+  });
+
+  test("should decrement the counter", () => {
+    render(<MyCounterApp />);
+
+    const labelH1 = screen.getByRole("heading", { level: 1 });
+    const button = screen.getByRole("button", { name: "-1" });
+
+    fireEvent.click(button);
+    expect(labelH1.innerHTML).toContain("counter: 9");
+  });
+  
+  test("should reset the counter", () => {
+    render(<MyCounterApp />);
+
+    const labelH1 = screen.getByRole("heading", { level: 1 });
+    const button = screen.getByRole("button", { name: "Reset" });
+
+    fireEvent.click(button);
+    expect(labelH1.innerHTML).toContain("counter: 10");
   });
 });
