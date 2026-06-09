9cbe1b30c8377696809e5d90d0e8f517690631fa
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2026-06-09 15:51:28 -0500

    gifsMock (testing)

---
+++ src/tests/mock/gifs.data.ts
@@ -0,0 +1,72 @@
+export const gifsMock = [
+  {
+    id: 'd5fMI9ftgQiGzoZoB9',
+    title: '',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/d5fMI9ftgQiGzoZoB9/giphy.gif',
+    width: 480,
+    height: 270
+  },
+  {
+    id: 'B6SyssSlTgPXq',
+    title: 'Dragonball Goku Super Saiyan GIF',
+    url: 'https://media0.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/B6SyssSlTgPXq/giphy.gif',
+    width: 200,
+    height: 200
+  },
+  {
+    id: 'eJ1U3jkPwvnGTcTiRz',
+    title: 'Dragon Ball Manga Edit GIF',
+    url: 'https://media3.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/eJ1U3jkPwvnGTcTiRz/giphy.gif',
+    width: 360,
+    height: 480
+  },
+  {
+    id: 'kvn0FeqyOXgohYNBNa',
+    title: 'Goku GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/kvn0FeqyOXgohYNBNa/giphy.gif',
+    width: 272,
+    height: 480
+  },
+  {
+    id: 'bPLlVHjnlfS6m3wIjg',
+    title: 'Look At You Dragon Ball Z GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/bPLlVHjnlfS6m3wIjg/giphy.gif',
+    width: 480,
+    height: 480
+  },
+  {
+    id: 'Bva8ZKa2Ww72BQKYWl',
+    title: 'Scream Rage GIF',
+    url: 'https://media2.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Bva8ZKa2Ww72BQKYWl/giphy.gif',
+    width: 480,
+    height: 270
+  },
+  {
+    id: 'GRkAAskXVavHwIbucG',
+    title: 'Phone Meme GIF',
+    url: 'https://media2.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/GRkAAskXVavHwIbucG/giphy.gif',
+    width: 480,
+    height: 406
+  },
+  {
+    id: 'vThGQEkz4f9eg',
+    title: 'dragon ball z 90s GIF',
+    url: 'https://media0.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/vThGQEkz4f9eg/giphy.gif',
+    width: 500,
+    height: 523
+  },
+  {
+    id: 'osm7o1nCvv7pR7AyGO',
+    title: 'Game Illustration GIF',
+    url: 'https://media0.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/osm7o1nCvv7pR7AyGO/giphy.gif',
+    width: 480,
+    height: 480
+  },
+  {
+    id: '7XiOILlURPg3SKb5eN',
+    title: 'Dragonball GIF',
+    url: 'https://media3.giphy.com/media/v1.Y2lkPTQwMzNhYmRkZW44NDcyb2JmdG15N3A0Y2pvZjFvdDczZmIwM21jdTNjbXRlMTJmeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/7XiOILlURPg3SKb5eN/giphy.gif',
+    width: 480,
+    height: 270
+  }
+]
