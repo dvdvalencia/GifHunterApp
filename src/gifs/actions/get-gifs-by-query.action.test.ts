4f5b3ed30c68761725880d3400177a1a31cda3e1
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2026-06-09 16:03:01 -0500

    get-gifs-by-query.action.test.ts pruevas de  lenght y data (testing)

--- src/gifs/actions/get-gifs-by-query.action.test.ts
+++ src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,10 +1,21 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { getGifsByQuery } from "./get-gifs-by-query.action";
 
 describe("getGifsByQuery", () => {
   test("should return a list of gifts", async () => {
     const gifs = await getGifsByQuery("Goku");
-    console.log(gifs);
-    
+    const [gif1] = gifs;
+
+    expect(gifs.length).toBe(10);
+
+    // expect(gif1).toEqual({
+    expect(gif1).toStrictEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      url: expect.any(String),
+      width: expect.any(Number),
+      height: expect.any(Number),
+    });
+    // console.log(gifs);
   });
 });
